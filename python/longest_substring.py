2d42e5d324a414223a7e5fae5a4a83d95c11b50b
sudhuke <46337231+sudhuke@users.noreply.github.com>
2019-04-05 09:51:45 +0530

    Solution accepted by leetcode
    Runtime: 104 ms, faster than 42.24% of Python3 online submissions for Longest Substring Without Repeating Characters.
    Memory Usage: 17.9 MB, less than 5.05% of Python3 online submissions for Longest Substring Without Repeating Characters.

--- python/longest_substring.py
+++ python/longest_substring.py
@@ -1,51 +1,48 @@
-string=input("Enter a string")
-length=len(string)
-print(length)
-count=0
-next_count=1
-string_list = list(string)
-substring = string[0]
-final_list = []
-for current_character in string_list:
-    print("checking for character : " + current_character)
-    if count < length:
-        #substring += current_character
-        if count + 1 == length:
-            next_character = string[count]
-        else:
-            next_character = string[count+1]
-        print("next character would be : " + next_character)
-        print("Index of character : ")
-        print(count)
-        if current_character == next_character and count != length:
-        # Same character found
-            final_list.append(substring)
-            substring = ""
-            print("Resetting the substring  ..." + substring)
-            count += 1
-            continue
-
-        else:
-            # if next_character not in substring:
-                if substring == "":
-                    substring += current_character + next_character
+class Solution:
+    def lengthOfLongestSubstring(self, str):
+        mystring = str
+        length = len(mystring)
+        print(length)
+        count = 0
+        next_count = 1
+        string_list = list(mystring)
+        if length == 0:
+            return 0
+        substring = mystring[0]
+        final_list = []
+        for current_character in string_list:
+            # print("checking for character : " + current_character)
+            if count < length:
+                # substring += current_character
+                if count + 1 == length:
+                    next_character = mystring[count]
                 else:
-                    if next_character in substring:
-                        substring += current_character
+                    next_character = mystring[count+1]
+                    # print("next character would be : " + next_character)
+                    # print("Index of character : ")
+                    # print(count)
+                if current_character == next_character and count != length:
+                    # Same character found
+                    substring = ""
+                    # print("Resetting the substring  ..." + substring)
+                    final_list.append(current_character)
+                    count += 1
+                    continue
+                else:
+                    # if next_character not in substring:
+                    if substring == "":
+                        substring += current_character + next_character
                     else:
-                        substring += next_character
+                        if next_character in substring and substring.index(next_character) < count:
+                            # print("Found a character: " + next_character +" in substring : " + substring)
+                            substring = substring[substring.index(next_character)+1:]+next_character
+                        else:
+                            substring += next_character
+                    count += 1
 
-    print("Appended the string : "+substring)
-    count += 1
-for entry in final_list:
-    print(entry)
-
-
-
-
-
-
-
-
-
-
+            #    print("Appended the string : "+substring)
+            final_list.append(substring)
+        # print(final_list)
+        result = max(final_list, key=len)
+        # print(len(result))
+        return len(result)
