8f17b1b4f370bc31ad8d1bd97112fcf6ea76f165
sudhuke <46337231+sudhuke@users.noreply.github.com>
2019-04-21 13:17:13 +0530

    Update median_two_sorted_arrays.py
    Success
    Details
    Runtime: 60 ms, faster than 97.02% of Python3 online submissions for Median of Two Sorted Arrays.
    Memory Usage: 13.4 MB, less than 5.11% of Python3 online submissions for Median of Two Sorted Arrays.

--- python/median_two_sorted_arrays.py
+++ python/median_two_sorted_arrays.py
@@ -1,31 +1,23 @@
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2) -> float:
         newlist = []
-        #newlist.append(nums1)
-        #newlist.append(nums2)
         for item in nums1:
             newlist.append(item)
 
         for item in nums2:
             newlist.append(item)
 
-        for item in newlist:
-            newlist.sort()
-            print(item)
+        newlist.sort()
         length = len(newlist)
         check_length = length % 2
-        #print("Length of the string: " + str(length))
-        #print(check_length)
         if check_length != 0:
             median_no = length / 2
             median = newlist[int(median_no)]
-            #print(median)
         else:
             right_index = int( length / 2 )
             left_index = int(right_index) - 1
             left_index_value = int(newlist[left_index])
             right_index_value = int(newlist[right_index])
             median = (left_index_value + right_index_value ) / 2
-            #print(new_median)
 
         return float(median)
